9c6310b3771790624d7b8e49afc2889d79cb5452
Fabian Mächler <fabian.maechler@gmaill.com>
2021-10-31 10:30:19 +0100

    f print prices parametrized

--- src/test/java/ch/ma3/alcor/testdoubles/BtcIndicator.java
+++ src/test/java/ch/ma3/alcor/testdoubles/BtcIndicator.java
@@ -1,7 +1,13 @@
 package ch.ma3.alcor.testdoubles;
 
 public class BtcIndicator {
-    public int printPrice() {
-        return 123;
+    private int price;
+
+    public BtcIndicator(int price) {
+        this.price = price;
+    }
+
+    public String printPrice() {
+        return price + "";
     }
 }
--- src/test/java/ch/ma3/alcor/testdoubles/BtcIndicatorShould.java
+++ src/test/java/ch/ma3/alcor/testdoubles/BtcIndicatorShould.java
@@ -2,13 +2,13 @@ package ch.ma3.alcor.testdoubles;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class BtcIndicatorShould {
-
-    private int actualPrice;
 
     /**
      * Requirements
@@ -20,11 +20,13 @@ public class BtcIndicatorShould {
     void setUp() {
     }
 
-    @Test
-    void printPriceOfBitcoin() {
-        actualPrice = new BtcIndicator().printPrice();
-        assertThat(actualPrice, is(123));
+    @ParameterizedTest
+    @CsvSource({
+            "123,'123'",
+            "421,'421'"
+    })
+    void printPriceOfBitcoin123(int givenPrice, String expectedPrint) {
+        String printed = new BtcIndicator(givenPrice).printPrice();
+        assertThat(printed, is(expectedPrint));
     }
-
-
 }
