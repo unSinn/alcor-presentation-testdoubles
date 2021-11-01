71f3685921681e48fa824605586cf4aa0a90a99e
Fabian Mächler <fabian.maechler@gmaill.com>
2021-11-01 11:04:47 +0100

    F use CoinGeckoApi

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,18 @@ repositories {
 }
 
 dependencies {
-    implementation("drewcarlson.coingecko:coingecko-jvm:0.1.6")
+    compileOnly("org.projectlombok:lombok:1.18.22")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
     testImplementation("org.junit.vintage:junit-vintage-engine:5.8.1")
     testImplementation("org.hamcrest:hamcrest:2.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     testImplementation("org.assertj:assertj-core:3.21.0")
-        testImplementation("org.mockito:mockito-junit-jupiter:4.0.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.0.0")
 }
 
 tasks.getByName<Test>("test") {
--- src/main/java/ch/ma3/alcor/btcprinter/adapters/CoingeckoBtcDataSource.java
+++ src/main/java/ch/ma3/alcor/btcprinter/adapters/CoingeckoBtcDataSource.java
@@ -4,12 +4,24 @@ import ch.ma3.alcor.btcprinter.internal.BtcData;
 import ch.ma3.alcor.btcprinter.internal.BtcDataSource;
 import ch.ma3.alcor.btcprinter.internal.Percent24h;
 import ch.ma3.alcor.btcprinter.internal.Price;
+import com.litesoftwares.coingecko.CoinGeckoApiClient;
+import com.litesoftwares.coingecko.constant.Currency;
+import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl;
 
+import java.util.Map;
 import java.util.Random;
 
 public class CoingeckoBtcDataSource implements BtcDataSource {
+
     public BtcData getData() {
-        Random random = new Random();
-        return new BtcData(new Price(random.nextInt(10000)), new Percent24h(random.nextFloat() + 0.5f));
+
+        CoinGeckoApiClient client = new CoinGeckoApiClientImpl();
+
+        float price24 = (float) client.getCoinById("bitcoin").getMarketData().getPriceChange24h();
+        int price = client.getCoinById("bitcoin").getMarketData().getCurrentPrice().get(Currency.CHF).intValue();
+
+        client.shutdown();
+
+        return new BtcData(new Price(price), new Percent24h(price24));
     }
 }
